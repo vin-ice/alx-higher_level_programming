9f1b02f7c58464380a4415bdb387e5d89909613c
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-12-02 00:19:25 +0300

    Correction-2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+from sys import argv
 if __name__ == "__main__":
-    from sys import argv
     argc = len(argv) - 1
     args = argv[1:]
     print("{} {}{}".format(argc, "argument" if argc == 1 else "arguments", ":"  if argc > 0 else "."))
