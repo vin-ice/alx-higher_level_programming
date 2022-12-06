ee3551c1e63d5c1ee3a370118c3bc8a04af9e1fe
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-12-06 21:41:59 +0300

    11-correction

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    size = len(my_list)
-    temp = [0] * (size - 1)
-    pos = 0
-    for index, item in enumerate(my_list):
-        if index == idx:
-            continue
-        temp[pos] = item
-        pos += 1
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    val = my_list[idx]
+    if my_list.index(val) == idx:
+        return my_list
+    copy = my_list.copy()
     my_list.clear()
-    for item in temp:
-        my_list.append(item)
+    for i, val in enumerate(copy):
+        if i == idx:
+            continue
+        my_list.append(val)
     return my_list
