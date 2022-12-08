41023ccdf9f5f8aaee13eb088d9aff997d6b4a24
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-12-08 10:06:06 +0300

    11-correction

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,4 +2,4 @@
 def multiply_list_map(my_list=[], number=0):
     if not my_list or len(my_list) == 0:
         return None
-    return list(map(lambda x: x * number, my_list))
+    return list(map((lambda x: x * number), my_list))
