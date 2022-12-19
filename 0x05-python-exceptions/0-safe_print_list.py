aaa7e4829d17b934a7a78d82290446051d6d38a1
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-12-20 00:02:15 +0300

    Fix-1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for i in range(x):
-            print(my_list[count], end="")
+        for i in list[:x]:
+            print("{}".format(i), end="")
             count += 1
     except IndexError:
         print("Accessed index is out of range")
