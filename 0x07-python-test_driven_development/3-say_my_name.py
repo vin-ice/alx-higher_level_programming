4e5216da1331054076c2877d45593c10ac5de56e
vin-ice <57555841+vin-ice@users.noreply.github.com>
2023-01-05 23:01:10 +0300

    3-correction

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,11 +3,11 @@
 
 def say_my_name(first_name, last_name=""):
     """Prints a pair of strings
-    
     Args:
         first_name (str): First name
         last_name (str): Last name
     """
+    
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
