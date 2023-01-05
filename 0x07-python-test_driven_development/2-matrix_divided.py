ae76c97307c2965825756f5711500e45d2362622
Vincent Wanjohi <vwan045@gmail.com>
2023-01-05 22:32:35 +0300

    2-correction

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,28 +9,16 @@ def matrix_divided(matrix, div):
     
     Returns:
         matrix_p (list): new list
-    """"
+    """
 
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
+    
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix) or not all(isinstance(cell, (int, float)) for row in matrix for cell in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     elif not isinstance(div, (float, int)):
         raise TypeError("div must be a number")
-    if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
+    elif not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
 
-    size, matrix_p = 0, []
-    for row in matrix:
-        if not isinstance(row, list) or all(not isinstance(item, (int, float)) for item in row):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-        if size: 
-            if size != len(row): 
-                raise TypeError("Each row of the matrix must have the same size")
-        else:
-            size = len(row)
-       
-        matrix_pp = []
-        for cell in row:
-            matrix_pp.append(round(cell / div, 2))
-        matrix_p.append(matrix_pp)
-    return matrix_p
+    return [[float("{:.2f}".format(cell / div)) for cell in row] for row in matrix]
