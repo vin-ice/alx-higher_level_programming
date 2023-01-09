86da602326ba516b102b003039c6f8c68930829e
vin-ice <57555841+vin-ice@users.noreply.github.com>
2023-01-09 23:26:38 +0300

    7-corr

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,8 +14,8 @@ class BaseGeometry:
             name (str): name of input
             value (int): integer value
         """
-        
-        if not isinstance(value, int):
+
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
