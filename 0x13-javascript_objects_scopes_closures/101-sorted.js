35dd0c2179f815d2c94de174e6cc78f82b30c6a8
Vincent Wanjohi <vwan045@gmail.com>
2023-03-14 20:33:05 +0300

    101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,8 +3,8 @@
 const dict = require('./101-data').dict;
 
 const newDict = Object.entries(dict).reduce((acc, [key, value]) => {
-    acc[value] = acc[value] ? [...acc[value], key] : [key];
-    return acc;
-  }, {});
-  
-  console.log(newDict);
+  acc[value] = acc[value] ? [...acc[value], key] : [key];
+  return acc;
+}, {});
+
+console.log(newDict);
